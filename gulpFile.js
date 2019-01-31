9b1ae66a2199a14e08acbc74ba8d11040cb23f37
EdgarJan <krodanix@gmail.com>
2019-01-31 13:38:12 +0200

    gulpFile fix

--- gulpFile.js
+++ gulpFile.js
@@ -2,33 +2,26 @@ const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const eslint = require('gulp-eslint');
 
-gulp.task('processing', function (done) {
+gulp.task('processing', () => {
 
   const stream = nodemon({
     script: 'gulp/processing',
     ext: 'js',
     watch: ['src'],
     tasks: ['lint'],
+    restartable: 'rs',
   });
 
   stream
     .on('restart', () => console.log('restarted!'))
     .on('crash', () => {
-      console.error('Application has crashed!\n')
+      console.error('Application has crashed!\n');
       // stream.emit('restart', 10)  // restart the server in 10 seconds
     });
 
 });
 
-gulp.task('lint', () => {
-  return gulp.src(['src/**/*.js'])
-  // eslint() attaches the lint output to the "eslint" property
-  // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.formatEach())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
-    .pipe(eslint.failAfterError());
-});
+gulp.task('lint', () => gulp.src(['src/**/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.formatEach())
+  .pipe(eslint.failAfterError()));
