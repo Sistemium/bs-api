213f2eb0e08e9f8fb7e3ced9145559ef1a7f451c
Alexander Levin <alevin.ru@gmail.com>
2019-02-09 22:23:34 +0200

    WATCH_DEBOUNCE var

--- src/processing/watchProcessing.js
+++ src/processing/watchProcessing.js
@@ -10,6 +10,15 @@ import { externalDb, processBox } from './processing';
 
 const { debug, error } = log('watchProcessing');
 
+const WATCH_DEBOUNCE = parseInt(process.env.WATCH_DEBOUNCE || 10000, 0);
+
+process.on('SIGINT', async () => {
+  error('SIGINT');
+  await mongo.disconnect()
+    .catch(error);
+  process.exit();
+});
+
 mongo.connect()
   .then(() => processing())
   .catch(e => error('mongo connect error', e.message));
@@ -18,33 +27,7 @@ function processing() {
 
   let processStarted = false;
 
-  const triggerProcessing = debounce(async () => {
-
-    if (processStarted) {
-      return;
-    }
-
-    processStarted = true;
-
-    try {
-      await externalDb.connect();
-      debug('external db connected');
-
-      debug('mongo connected');
-
-      await marksProcessing(processBox, args => externalDb.exportMark(args));
-      debug('finish');
-
-      await externalDb.disconnect();
-
-      debug('disconnected');
-    } catch (e) {
-      error(e);
-    }
-
-    processStarted = false;
-
-  }, 30000);
+  const triggerProcessing = debounce(doProcessing, WATCH_DEBOUNCE);
 
   ArticleDoc.watch()
     .on('change', triggerProcessing);
@@ -52,5 +35,35 @@ function processing() {
     .on('change', triggerProcessing);
   EgaisMark.watch()
     .on('change', triggerProcessing);
+
+  triggerProcessing();
+
+  async function doProcessing() {
+
+    if (processStarted) {
+      debug('exit already started');
+      return;
+    }
+
+    processStarted = true;
+
+    try {
+
+      await externalDb.connect();
+      debug('external db connected');
+
+      await marksProcessing(processBox, args => externalDb.exportMark(args));
+      debug('finish');
+
+      await externalDb.disconnect();
+      debug('external db disconnected');
+
+    } catch (e) {
+      error(e);
+    }
+
+    processStarted = false;
+
+  }
 
 }
